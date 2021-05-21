1cf86407b99bbcd4d6c7e55d449814e182b6476e
Simone Balzan <developer.desenv@gmail.com>
2021-05-21 04:31:30 -0300

    Imagem adicionada

--- App.js
+++ App.js
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
 
-  function handleSubmit(){
+  function handleSubmit() {
     const alt = altura / 100;
     const imc = peso / (alt * alt);
-    
 
-    if(imc < 18.6){
+
+    if (imc < 18.6) {
       alert('Você está abaixo do peso!' + imc.toFixed(2));
-      }else if(imc >= 18.6 && imc < 24.9){
-        alert('Peso ideal! ' + imc.toFixed(2));
-      }else if(imc >= 24.9 && imc < 34.9){
-        alert('Você está levemente acima do peso!' + imc.toFixed(2));
-      }
+    } else if (imc >= 18.6 && imc < 24.9) {
+      alert('Peso ideal! ' + imc.toFixed(2));
+    } else if (imc >= 24.9 && imc < 34.9) {
+      alert('Você está levemente acima do peso!' + imc.toFixed(2));
+    }
   }
   return (
     <View style={styles.container}>
+      <Image style={styles.imagem} source={require('./assets/img/nv.png')} />
       <Text style={styles.title}> Calcule seu IMC</Text>
-
       <TextInput
         style={styles.input}
         value={peso}
-        onChangeText={ (peso) => setPeso(peso) }
+        onChangeText={(peso) => setPeso(peso)}
         placeholder="Peso(Kg)"
         keyboardType="numeric"
       />
@@ -33,16 +33,16 @@ export default function App() {
       <TextInput
         style={styles.input}
         value={altura}
-        onChangeText={ (altura) => setAltura(altura) }
+        onChangeText={(altura) => setAltura(altura)}
         placeholder="Altura (cm)"
         keyboardType="numeric"
       />
-      <TouchableOpacity 
-      style={styles.button}
-      onPress={handleSubmit}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleSubmit}
       >
-       
-        <Text style= {styles.buttonText}> Calcular </Text>
+
+        <Text style={styles.buttonText}> Calcular </Text>
       </TouchableOpacity>
 
     </View>
@@ -65,16 +65,23 @@ const styles = StyleSheet.create({
     color: '#FFF',
     fontSize: 23,
   },
-  button:{
-    justifyContent:'center',
-    alignItems:'center',
-    margin:15,
-    backgroundColor:'#41Aef4',
-    padding:10,
-     },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#41Aef4',
+    padding: 10,
+  },
 
-     buttonText:{
-       color:'#FFF',
-       fontSize: 25,
-     },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 25,
+  },
+  imagem: {
+    margin:100,
+    width: 200,
+    height: 200,
+    alignItems: 'center',
+  }
+
 });
